c75e06cbfd67d98dcdad77e02ded996745363d65
Josaphat-PRIME <bakaijosaphat@gmail.com>
2026-08-14 18:54:06 +0200

    setting IDs of servos

--- Config_origin.py
+++ Config_origin.py
@@ -5,6 +5,7 @@ servo = ST3215("/dev/ttyAMA3")
 servo_ID= int (input ("Choose your servo ID: "))
 print("This actual position is : ", servo.ReadPosition(servo_ID))
 while True :
+    print(servo.ReadStatus(servo_ID))
     angle = int (input ("give an angle : "))
     servo.WritePosition(servo_ID, angle)
     print("task Done ...")
---
+++ Set_IDs.py
@@ -0,0 +1,28 @@
+### Before start make shure that only one servo is connected to the servo driver 
+
+from st3215 import ST3215
+
+servo = ST3215("/dev/ttyAMA3")  # make shure to choose the right UART port
+
+print("Starting ...")
+
+old_ID= servo.ListServos()
+
+print("Current ID is : ",old_ID[0])
+
+New_ID = int(input("Type the new ID and press ENTER "))
+
+if ((New_ID > 0) and (New_ID < 254) ) :
+    print("Changing ID ..." )
+    servo.ChangeId(old_ID[0], New_ID)
+    current_ID= servo.ListServos()
+    print("Checking update ..." )
+    if (current_ID[0] == New_ID):
+        print("the new_ID is : " ,  New_ID )
+        print( "Task Done ")
+
+    else :
+        print("Error occured, chech your wiring or your UART port")
+
+else : 
+    print("Your ID must be in range of 1-254 ")
