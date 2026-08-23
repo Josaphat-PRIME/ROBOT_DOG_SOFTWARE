44cfd916307ec3136d7be75cbb6738f24ba21252
Josaphat-PRIME <bakaijosaphat@gmail.com>
2026-08-24 00:21:31 +0200

    finilizing the Freudeinstein formula

--- LOCOMOTION/Inverse_Kinematics.py
+++ LOCOMOTION/Inverse_Kinematics.py
@@ -1,20 +1,43 @@
 import math
 
+# variables and legs parameters
+lower_leg = 110
+upper_leg = 100
+
+a = 30
+b = 100
+c = 20
+d = upper_leg
+
+k1 = d / a
+k2 = d / c
+k3 = ( (d**2)+(a**2)-(b**2)+(c**2) )/(2*c*a)
+
 while True :
 
     x , z = float (input ("entrez la valeur de de X : ")) , float (input ("entrez la valeur de de Z : "))
 
-    a = 110
-    b = 100
     r = math.sqrt((x**2)+(z**2))
 
-    if (abs(a-b) <= r <= a+b ) :
-        Theta_2 = math.acos((((a**2)+(b**2))-(r**2))/(2*a*b))
-        Theta_1 =  math.acos((((b**2)+(r**2)-(a**2))/(2*b*r))) - math.atan2(z , x)
+    if (abs(lower_leg-upper_leg) <= r <= lower_leg+upper_leg ) :
+        Theta_2 = math.acos((((lower_leg**2)+(upper_leg**2))-(r**2))/(2*lower_leg*upper_leg))
+        Theta_1 =  math.acos((((upper_leg**2)+(r**2)-(lower_leg**2))/(2*upper_leg*r))) + math.atan2(x , z) - (math.pi/2)
 #(math.pi) +
+
+    #----------------------------------------------------------------------
+        A = k2 + math.cos(Theta_2)
+        B = math.sin(Theta_2)
+        C = k3 + k1*math.cos(Theta_2)
+        R = math.sqrt((A**2)+(B**2))
+        phi = math.atan2(B,A) 
+        Theta_e = abs(phi + math.acos(C/R))
+    #----------------------------------------------------------------------
+        Theta_e = math.degrees(Theta_e)
         Theta_2 = math.degrees(Theta_2)
         Theta_1 = math.degrees(Theta_1) % 360 # to normalize the angle (0 < theta_2 < 360)
+       
 
+        print("valeur de Theta_e : " , Theta_e)
         print("valeur de Theta_1 : " , Theta_1)
         print("valeur de Theta_2 : " , Theta_2)
 
